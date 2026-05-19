bfa823c0933f6f1596d67f67a38cf1ceb8b1c232
Nam Khagg <namkhang0807@gmail.com>
2026-05-20 00:17:22 +0700

    add filter by done

--- todo_app_v1/app.js
+++ todo_app_v1/app.js
@@ -2,9 +2,14 @@ const input = document.querySelector(".todo-field");
 const addButton = document.querySelector(".add-btn");
 const list = document.querySelector(".todo-list");
 
-console.log(addButton);
+const filterList = document.querySelectorAll("span");
+const allFilterButton = document.querySelector(".all");
+const inProgressButton = document.querySelector(".in-progress");
+const doneFilter = document.querySelector(".done");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+let filterStatus = "all";
 
 addButton.addEventListener("click", () => {
     const data = input.value.trim();
@@ -22,15 +27,36 @@ addButton.addEventListener("click", () => {
     render();
 })
 
+doneFilter.addEventListener("click", () => {
+    filterStatus = "done";
+    updateData(doneFilter);
+
+    render();
+})
+
 
 function saveData() {
     localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function updateData(element) {
+    filterList.forEach((item) => {
+        item.classList.remove("active");
+    })
+
+    element.classList.add("active");
 }
 
 function render() {
     let filterTodos = todos;
 
     list.innerHTML = "";
+
+    if (filterStatus === "done") {
+        filterTodos = filterTodos.filter((item) => {
+            return item.isCompleted === true;
+        })
+    }
 
     filterTodos.forEach((todo) => {
         const li = document.createElement("li");
@@ -44,13 +70,24 @@ function render() {
         `
         list.appendChild(li);
 
+        const delButton = li.querySelector(".del-btn");
+
+        delButton.addEventListener("click", () => {
+            console.log(delButton);
+            todos = todos.filter((item) => {
+                return todo.id !== item.id;
+            })
+
+            saveData();
+            render();
+        })
+
         const checkbox = li.querySelector(".checkbox");
 
-        
         checkbox.addEventListener("click", () => {
             console.log(checkbox);
             todos = todos.map((item) => {
-                if(item.id == todo.id) {
+                if(item.id === todo.id) {
                     return {
                         ...item,
                         isCompleted : !item.isCompleted
